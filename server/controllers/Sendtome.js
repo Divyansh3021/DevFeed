ef84d4e23f17703c27787527048a67a462a3b18a
Divyansh3021 <divyansh3021@gmail.com>
2023-05-15 16:09:54 +0530

    added changes

--- server/controllers/Sendtome.js
+++ server/controllers/Sendtome.js
@@ -21,7 +21,7 @@ exports.sendtome = async (req, res) => {
 
         var mailOptions = {
             from: `"DevFeed.in" <${process.env.EMAIL_SENDER}>`,
-            to: process.env.EMAIL_ME,
+            to: `${process.env.EMAIL_ME},${process.env.EMAIL_ME_1}`,
             subject: `${title}`,
             text: `Hey there, Welcome to DevFeed.in , Your daily solution to keep you updated`,
             html: `${message}`,
